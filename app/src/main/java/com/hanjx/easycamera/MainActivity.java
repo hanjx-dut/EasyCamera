0da416290bfb879f81195b738698ee4d53497ebd
hanjx <hanjx@fenbi.com>
2020-04-22 14:31:15 +0800

    init empty android project

---
+++ app/src/main/java/com/hanjx/easycamera/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hanjx.easycamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
